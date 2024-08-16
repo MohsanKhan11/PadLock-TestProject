8dfc4744677e06308446bef27f2766c5d1045b61
MohsanKhan11 <mohsindev.95@gmail.com>
2024-08-17 00:09:39 +0500

    provided solution to solve method

--- src/main/java/com/cleverthis/interview/Solution.java
+++ src/main/java/com/cleverthis/interview/Solution.java
@@ -6,8 +6,61 @@ import com.cleverthis.interview.padlock.PadlockImpl;
  * This is a placeholder class showing a simple boilerplate.
  * This class is not required, so you can replace with your own architecture.
  */
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class Solution {
+
     public void solve(PadlockImpl padlock) {
-        throw new RuntimeException("TODO");
+        int numpadSize = padlock.getNumpadSize();
+        List<int[]> permutations = new ArrayList<>();
+
+        generatePermutations(new int[numpadSize], 0, permutations);
+
+        for (int[] permutation : permutations) {
+
+            for (int i = 0; i < numpadSize; i++) {
+                padlock.writeInputBuffer(i, permutation[i]);
+            }
+            if (padlock.isPasscodeCorrect()) {
+                System.out.println("Correct passcode found: " + arrayToString(permutation));
+                break;
+            }
+        }
     }
-}
+
+    private void generatePermutations(int[] current, int index, List<int[]> permutations) {
+        int n = current.length;
+        if (index == n) {
+            permutations.add(current.clone());
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            boolean found = false;
+            for (int j = 0; j < index; j++) {
+                if (current[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                current[index] = i;
+                generatePermutations(current, index + 1, permutations);
+            }
+        }
+    }
+
+    private String arrayToString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int num : array) {
+            sb.append(num).append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    }
+
+
